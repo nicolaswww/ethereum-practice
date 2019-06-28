dd53e9e25c0e53e648ef0b860903a789d1f5e7f1
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-06-27 21:07:13 -0300

    add join and getUser functions

--- contracts/UsersContract.sol
+++ contracts/UsersContract.sol
@@ -8,5 +8,32 @@ contract UsersContract {
         string surName;
     }
 
+    // coleccion de usuarios (mapping): permite enlazar direcciones de ethereum a usuarios. Para cada direccion representaremos un usuario
+    mapping(address => User) private users;
+
+    // con esta funcion los usuarios van a poder registrarse para pasar a formar parte de nuestro mapping de usuarios
+    function join(string name, string surName) public {
+        // usamos address de la persona que está enviando la transacción
+        User storage user = users[msg.sender];
+        user.name = name;
+        user.surName = surName;
+
+        /*
+        Utilizando memory se podrían realizar cambios en memoria que solo afectarían a este scope:
+        User memory user = users[msg.sender];
+        user.name = name;
+        user.surName = surName;
+
+        Si se utiliza storage los cambios realizados si persisten.
+        */
+    }
+
+    // devuelve nombre y apellido del usuario que hace la transacción
+    // view: solamente recupera datos, no hace modificaciones
+    function getUser(address addr) public view returns (string, string) {
+        // se usa memory ya que solo se consultan datos, no se modifican
+        User memory user = users[addr];
+        return (user.name, user.surName);
+    }
 
 }

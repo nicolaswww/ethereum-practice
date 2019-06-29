0397fe82290796abddd5881f47dac1e940ec0a3e
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-06-29 16:10:36 -0300

    add error messages. syntax (event UpperCamelCase)

--- contracts/UsersContract.sol
+++ contracts/UsersContract.sol
@@ -2,66 +2,59 @@ pragma solidity ^0.4.24;
 
 contract UsersContract {
 
-    // estructura de contrato
     struct User {
         string name;
         string surName;
     }
 
-    // coleccion de usuarios (mapping): permite enlazar direcciones de ethereum a usuarios. Para cada direccion representaremos un usuario
     mapping(address => User) private users;
     mapping(address => bool) private joinedUsers;
     address[] total;
 
-    event onUserJoined(address, string);
+    event OnUserJoined(address, string);
 
-    // con esta funcion los usuarios van a poder registrarse para pasar a formar parte de nuestro mapping de usuarios
+    /**
+     * users join
+     * @param name {string} - user name
+     * @param surName {string} - user surname
+     */
     function join(string name, string surName) public {
-        // valida que el usuario no se haya registrado previamente
-        require(!userJoined(msg.sender));
+        require(!userJoined(msg.sender), "Account already joined");
 
-        // usamos address de la persona que está enviando la transacción
-        // la key (msg.sender) viene representado en la address
         User storage user = users[msg.sender];
         user.name = name;
         user.surName = surName;
 
-        /*
-        Utilizando memory se podrían realizar cambios en memoria que solo afectarían a este scope:
-        User memory user = users[msg.sender];
-        user.name = name;
-        user.surName = surName;
-
-        Si se utiliza storage los cambios realizados si persisten.
-        */
-
-        // guarda usuarios registrados para evitar que vuelvan a hacerlo
         joinedUsers[msg.sender] = true;
-
-        // registro de array de todas las direcciones
         total.push(msg.sender);
-
-        // emite evento al unirse un usuario
-        emit onUserJoined(msg.sender, string(abi.encodePacked(name, " ", surName)));
+        emit OnUserJoined(msg.sender, string(abi.encodePacked(name, " ", surName)));
     }
 
-    // devuelve nombre y apellido del usuario que hace la transacción
-    // view: solamente recupera datos, no hace modificaciones
+    /**
+     * get user by address
+     * @param addr {string} - user address
+     * @return {string} - full user name
+     */
     function getUser(address addr) public view returns (string, string) {
-        // valida que el usuario esté registrado
-        require(userJoined(msg.sender));
+        require(userJoined(msg.sender), "Account must be join");
 
-        // se usa memory ya que solo se consultan datos, no se modifican
         User memory user = users[addr];
         return (user.name, user.surName);
     }
 
-    // retorna si la dirección ha sido registrada
+    /**
+     * check if the user joined
+     * @param addr {string} - user address
+     * @return {boolean} - user are joined
+     */
     function userJoined(address addr) private view returns (bool) {
         return joinedUsers[addr];
     }
 
-    // retorna cantidad de usuarios
+    /**
+     * return users quantity
+     * @return {number} - users
+     */
     function totalUsers() public view returns (uint) {
         return total.length;
     }
